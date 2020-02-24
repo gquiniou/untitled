83f5c83c2069598bcbfc18482e812df8c88b4061
gquiniou <gquiniou@gmail.com>
2020-02-24 21:07:33 +0100

    X

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Event.hpp>
+#include <SFML/OpenGL.hpp>
 
 
 int main() {
@@ -10,19 +11,26 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "untitled", sf::Style::Close);
     window.setMouseCursorVisible(false);
     window.setVerticalSyncEnabled(true);
-    window.display();
+
+
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    printf("%u\n", vertexBuffer);
 
     while (window.isOpen()) {
         sf::Event event{};
         while (window.pollEvent(event)) {
-
             if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)) {
                 window.close();
             }
             if (event.type == sf::Event::Closed) {
                 window.close();
+            } else if (event.type == sf::Event::Resized) {
+                glViewport(0, 0, event.size.width, event.size.height);
             }
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            window.display();
         }
     }
-        return 0;
+    return 0;
 }
